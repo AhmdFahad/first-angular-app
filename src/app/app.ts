8af432203929024dd04ac95ec196a7584fe7f9b0
AhmdFahad <ahmd66699@gmail.com>
2025-06-19 12:24:05 +0300

    play around and create basic component
    basic component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'first-angular-app';
+  name: string = 'Ahmed';
 }
